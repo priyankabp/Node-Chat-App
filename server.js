886b6ba42faad0e66c8e9c30e73d70d63236b433
Priyanka Phapale <pbphapale@gmail.com>
2018-10-02 16:49:42 -0400

    Display messages returned from backend

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
+
+var messages = [
+    { name: 'Tim', message: 'Hi' },
+    { name: 'Jane', message: 'Hello' },
+]
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
 var server = app.listen(3000, () => {
     console.log('Server is listing on port', server.address().port)
 });
