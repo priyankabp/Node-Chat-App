b04e76c622baa5a18c40c3925e5caf398ee0aafa
Priyanka Phapale <pbphapale@gmail.com>
2018-10-02 17:11:57 -0400

    Get and post msg from and to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var messages = [
     { name: 'Tim', message: 'Hi' },
@@ -8,6 +12,11 @@ var messages = [
 ]
 app.get('/messages', (req, res) => {
     res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
 })
 
 var server = app.listen(3000, () => {
