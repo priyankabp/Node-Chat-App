cac06cec7dc717f21a13514d0eb10eb8be6c4e0e
Priyanka Phapale <pbphapale@gmail.com>
2018-10-03 12:53:15 -0400

    Setup MongoDB and mongoose

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http)
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+var dbUrl = 'mongodb://dbuser:dbuser1@ds121753.mlab.com:21753/learning-node'
 
 var messages = [
     { name: 'Tim', message: 'Hi' },
@@ -24,6 +27,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user conected');
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('Mongodb Connection', err);
 })
 
 var server = http.listen(3000, () => {
